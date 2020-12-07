88c23ebce77700f7671eb9b9de3729c1025a13dc
xysszwx <2909420436@qq.com>
2020-12-07 09:15:24 +0800

    评论功能 子评论添加

---
+++ blog/src/main/java/com/zwx/blog/pojo/Comment.java
@@ -0,0 +1,133 @@
+package com.zwx.blog.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class Comment implements Serializable {
+    private Long id;
+
+    private String avatar;
+
+    private String content;
+
+    private Date createTime;
+
+    private String email;
+
+    private String nickname;
+
+    private Long blogId;
+
+    private Long parentCommentId;
+
+    private List<Comment> replyComment;
+
+    public List<Comment> getReplyComment() {
+        return replyComment;
+    }
+
+    public void setReplyComment(List<Comment> replyComment) {
+        this.replyComment = replyComment;
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    public Comment(Long id, String avatar, String content, Date createTime, String email, String nickname, Long blogId, Long parentCommentId) {
+        this.id = id;
+        this.avatar = avatar;
+        this.content = content;
+        this.createTime = createTime;
+        this.email = email;
+        this.nickname = nickname;
+        this.blogId = blogId;
+        this.parentCommentId = parentCommentId;
+    }
+
+    public Comment() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar == null ? null : avatar.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public Long getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(Long blogId) {
+        this.blogId = blogId;
+    }
+
+    public Long getParentCommentId() {
+        return parentCommentId;
+    }
+
+    public void setParentCommentId(Long parentCommentId) {
+        this.parentCommentId = parentCommentId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", avatar=").append(avatar);
+        sb.append(", content=").append(content);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", email=").append(email);
+        sb.append(", nickname=").append(nickname);
+        sb.append(", blogId=").append(blogId);
+        sb.append(", parentCommentId=").append(parentCommentId);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
